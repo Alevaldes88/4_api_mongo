7ea61caf73fefd0fab95d2e29988f4e0d954af9d
Alejandro Valdés <alejandrovaldesp86@gmail.com>
2025-07-21 14:02:47 +0200

    Controlador actualizar implementado

--- controllers/book_controllers.py
+++ controllers/book_controllers.py
@@ -52,3 +52,20 @@ async def get_book_by_id(book_id: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
     
+# controlador actualiozar un libro por su id 
+
+async def update_book(book_id: str, book_data: BookCreate):
+    try:
+        if not ObjectId.is_valid(book_id):
+            raise HTTPException(status_code=400, detail='El id del libro no es válido')
+        result= await book_collection.update_one(
+            {"_id": ObjectId(book_id)},
+            {"$set": book_data.model_dump()}
+            )
+        if result.modified_count == 0:
+            return None
+        update = await book_collection.find_one({"_id": ObjectId(book_id)})
+        return book_helper(update)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
+    
