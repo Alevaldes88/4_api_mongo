f3e33e16d5578cb8ad7e440da5dda30787435ba5
Alejandro Valdés <alejandrovaldesp86@gmail.com>
2025-07-21 10:46:34 +0200

    He creado main.py para levantar fastapi, el fichero rutas y modelo

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastApi
+from routes import book_routes
+
+app = FastApi()
+
+# Ruta de acceso a los libros
+
+app.include_router(book_routes.router,
+                   prefix = "/books"
+                   tags=["Books"])
